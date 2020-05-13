3e1c35d7326427e05b46873ab16d16b1c5030419
Pushpendra Kumar <pushpendra.gangwar@outlook.com>
2020-05-13 22:48:27 +0200

    Removed debug prints

--- json2csv.py
+++ json2csv.py
@@ -11,18 +11,11 @@ printList = 0
 
 outputDict = collections.OrderedDict()
 
-def JSON2CSV_TRACE(formatedString):
-    #print(formatedString)
-    pass
-
 def unknownError(lastItem, error):
     print("Unkown error occurred at line=" + str(lineCounter) +
             "\nErrorDesc: " + error + "\nLastItem: " + str(lastItem),
             file=sys.stderr)
     exit()
-
-#JSON2CSV_TRACE(data)
-#JSON2CSV_TRACE(type(data))
 
 def handleList(inputList, outputDict, printListCount):
     global printList
@@ -33,7 +26,6 @@ def handleList(inputList, outputDict, printListCount):
         newDict = outputDict
         if (len(inputList) > 1 and bool(outputDict)):
             # Make copy of previous list and pass into next call
-            JSON2CSV_TRACE("Making copy")
             newDict = copy.deepcopy(outputDict)
         if type(item) is dict:
             handlDictType(item, newDict, printListCount)
@@ -55,30 +47,21 @@ def addToDict(key, value, dict):
 
 
 def handlDictType(inputDict, outputDict, printListCount):
-    JSON2CSV_TRACE(outputDict)
     global printList
     for key in inputDict:
         if type(inputDict[key]) is dict:
-            JSON2CSV_TRACE("handlDictType dict type" + " key=" + key)
             handlDictType(inputDict[key], outputDict, printListCount)
         elif type(inputDict[key]) is list:
             if ((type(inputDict[key][0]) is int) or (type(inputDict[key][0]) is str)):
-                JSON2CSV_TRACE("handlDictType list in list type")
                 valueArray = ' '.join([str(strOrInt) for strOrInt in inputDict[key]])
                 addToDict(key, valueArray, outputDict)
             else:
-                JSON2CSV_TRACE("handlDictType list type" + " key=" + key + " elements=" + str(len(inputDict[key])))
                 handleList(inputDict[key], outputDict, printListCount*len(inputDict[key]))
         elif type(inputDict[key]) is str:
-            JSON2CSV_TRACE("handlDictType appending value")
-            JSON2CSV_TRACE(inputDict[key])
             addToDict(key, inputDict[key], outputDict)
         else:
-            JSON2CSV_TRACE("handlDictType appending value")
-            JSON2CSV_TRACE(inputDict[key])
             addToDict(key, str(inputDict[key]), outputDict)
 
-    JSON2CSV_TRACE(outputDict)
     if printList:
         print(','.join(outputDict.values()))
         printList = 0
