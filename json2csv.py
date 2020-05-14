21bc0db5de4f666f5a67e9307395137d6e9fd8fc
ekmrpsh <pushpendra.kumar@ericsson.com>
2020-05-14 19:26:35 +0200

    Error handling and some more options added

--- json2csv.py
+++ json2csv.py
@@ -7,14 +7,20 @@ import argparse
 
 lineCounter = 0
 lastItemIsList = 0
+outputFile_fp = sys.stdout
 
 outputDict = collections.OrderedDict()
 
-def unknownError(lastItem, error):
-    print("Unkown error occurred at line=" + str(lineCounter) +
-            "\nErrorDesc: " + error + "\nLastItem: " + str(lastItem),
+def handleError(currentItem, error, exit):
+    print("***Error*** Line=" + str(lineCounter) +
+            "\nDesc=" + error + "\nCurrentItem=" + str(currentItem),
             file=sys.stderr)
-    exit()
+    if exit:
+        exit()
+
+def printOutput(outTrace):
+    global outputFile_fp
+    print(outTrace, file=outputFile_fp)
 
 def handleListType(inputList, outputDict):
     for item in inputList:
@@ -25,10 +31,10 @@ def handleListType(inputList, outputDict):
         if type(item) is dict:
             handleDictType(item, newDict)
             # Let's print after handling list element
-            print(','.join(newDict.values()))
+            printOutput(','.join(newDict.values()))
         else:
             # Not expected a list, str, number type item
-            unknownError(item, "Didn't expect list,int,srt type in handleListType()")
+            handleError(item, "Didn't expect list,int,srt type in handleListType()", True)
 
 def addToDict(key, value, dict):
     appendIndex = 1
@@ -63,21 +69,38 @@ def toCsv(inputJson):
     global lastItemIsList
     handleDictType(inputJson, outputDict)
     if not lastItemIsList:
-        print(','.join(outputDict.values()))
+        printOutput(','.join(outputDict.values()))
 
-def parsingStart(jsonDataFile):
+def startParsing(args):
     global lineCounter
+    global outputFile_fp
     # Open JSON data file
-    jsonDataFile_fp = open(jsonDataFile, "r")
+    jsonDataFile_fp = open(args.inputJsonFile, "r")
+
+    # if we have to write output file
+    if args.outputFile:
+        print(args.outputFile)
+        outputFile_fp = open(args.outputFile, "w")
 
     while True:
-        jsonObj = jsonDataFile_fp.readline()
+        jsonObjLine = jsonDataFile_fp.readline()
         lineCounter += 1
-        if jsonObj:
-            toCsv(json.loads(jsonObj))
+        if jsonObjLine:
+            try:
+                toCsv(json.loads(jsonObjLine))
+            except json.JSONDecodeError:
+                handleError(jsonObjLine, "json.JSONDecodeError", False)
             outputDict.clear()
         else:
             break
+
+    #Let's close output file so that it gets flushed, and wish that
+    # we have something useful in this :P
+    if args.outputFile:
+        outputFile_fp.close()
+
+    #And be a gentleman, not sure if python does it automatically
+    jsonDataFile_fp.close()
 
 # Summon the magic
 if __name__ == '__main__':
@@ -86,15 +109,25 @@ if __name__ == '__main__':
             epilog="",
             formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument(
-            '-j',
-            '--jsonDataFile',
-            metavar = '<JSON data file>',
-            help = 'JSON data file input is mandotory !')
+            'inputJsonFile',
+            metavar='<inputFile.json>',
+            help=''
+            )
+    parser.add_argument(
+            '-f',
+            '--outputFile',
+            metavar = '<outputFile.csv>',
+            help = '')
+    parser.add_argument(
+            '-c',
+            '--csvColumns',
+            metavar = '<"col1,col2,col3 ...">',
+            help = 'Column name should exactly match, in JSON objects. Columns printed in output CSV file only make sense if input JSON file have all lines of same JSON object type')
 
     args = parser.parse_args()
 
-    if not args.jsonDataFile:
+    if not args.inputJsonFile:
         parser.print_help()
     else:
-        parsingStart(args.jsonDataFile)
+        startParsing(args)
 
